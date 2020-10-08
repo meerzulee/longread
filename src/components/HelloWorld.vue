204064a224ca37be6e0c34130ff629e3ef352184
Meerzulee <meerzulee@gmail.com>
2020-10-08 16:01:04 +0600

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+   <h1 class="text-3xl text-teal-500 mt-8 flex items-center font-semibold ">Tailwind.CSS + <span class="vue font-bold mx-2"> Vue.JS</span> = ❤️</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.vue{
+   color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative flex flex-col items-center bg-purple-900 home">
+    <video muted loop autoplay class="absolute inset-0 z-10 bg-gray-900">
+      <source src="@/assets/03.webm" type="video/webm" />
+    </video>
+    <video muted loop autoplay class="absolute inset-0 z-20">
+      <source src="@/assets/02.webm" type="video/webm" />
+    </video>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld,
+  },
+}
+</script>
