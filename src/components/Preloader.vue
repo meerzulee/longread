d1788f2298b53105dbe02e36f94cd23a88fd4be1
Mirzaali Ruzimatov <ruzimatov_m@auca.kg>
2020-12-29 11:50:03 +0600

    Fix preloader

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,119 +1,121 @@
 <template>
-  <transition name="fade" mode="in-out">
-    <div
-      v-if="!this.$store.state.loaded"
-      class="absolute h-screen inset-0 bg-primary-dark w-screen flex items-center justify-center preloader-z"
-    >
-      <IntersectingCirclesSpinner :size="100" color="#CD6EF0" />
-      <div v-if="!isLoaded">
+  <div>
+    <transition name="fade" mode="in-out">
+      <div
+        v-if="!this.$store.state.loaded"
+        class="absolute h-screen inset-0 bg-primary-dark w-screen flex items-center justify-center preloader-z"
+      >
+        <IntersectingCirclesSpinner :size="100" color="#CD6EF0" />
+      </div>
+    </transition>
+    <div v-if="!isLoaded">
+      <!-- stage 1 -->
+      <div class="hidden" v-if="!stage1Loaded">
         <!-- stage 1 -->
-        <div class="hidden" v-if="!stage1Loaded">
-          <!-- stage 1 -->
-          <img
-            :src="require('@/assets/images/backgrounds/' + s1 + '.jpg')"
-            v-for="(s1, index) in 4"
-            @load="loadStage1('img ' + s1)"
-            :key="'S1' + index"
-            alt=""
-          />
-          <video
-            :src="require('@/assets/videos/webm/' + v1 + '.webm')"
-            v-for="(v1, index) in 4"
-            @loadeddata="loadStage1('video ' + v1)"
-            :key="'V1' + index"
-            alt=""
-            muted
-            preload="auto"
-          />
-          <video
-            :src="require('@/assets/videos/webm/intro.webm')"
-            @loadeddata="loadStage1('intro')"
-            alt=""
-            muted
-            preload="auto"
-          />
+        <img
+          :src="require('@/assets/images/backgrounds/' + s1 + '.jpg')"
+          v-for="(s1, index) in 4"
+          @load="loadStage1('img ' + s1)"
+          :key="'S1' + index"
+          alt=""
+        />
+        <video
+          :src="require('@/assets/videos/webm/' + v1 + '.webm')"
+          v-for="(v1, index) in 4"
+          @loadeddata="loadStage1('video ' + v1)"
+          :key="'V1' + index"
+          alt=""
+          muted
+          preload="auto"
+        />
+        <video
+          :src="require('@/assets/videos/webm/intro.webm')"
+          @loadeddata="loadStage1('intro')"
+          alt=""
+          muted
+          preload="auto"
+        />
 
-          <video
-            :src="require('@/assets/videos/intro.webm')"
-            @loadeddata="loadStage1('introbg')"
-            alt=""
-            muted
-            preload="auto"
-          />
-          <video
-            :src="require('@/assets/videos/webm/song.webm')"
-            @loadeddata="loadStage1('song')"
-            alt=""
-            muted
-            preload="auto"
-          />
-        </div>
-        <!-- stage 2 -->
-        <div class="hidden" v-if="stage1Loaded">
-          <img
-            :src="require('@/assets/images/backgrounds/' + (s2 + 4) + '.jpg')"
-            v-for="(s2, index) in 5"
-            @load="loadStage2('img ' + (s2 + 4))"
-            :key="'S2' + index"
-            alt=""
-          />
-          <video
-            :src="require('@/assets/videos/webm/' + (v2 + 4) + '.webm')"
-            v-for="(v2, index) in 5"
-            @loadeddata="loadStage2('video ' + (v2 + 4))"
-            :key="'V2' + index"
-            alt=""
-            muted
-            preload="auto"
-          />
-          <video
-            :src="require('@/assets/videos/thumbs/' + t + '.mp4')"
-            v-for="(t, index) in 13"
-            @loadeddata="loadStage2('thumbs ' + t)"
-            :key="'t' + index"
-            alt=""
-            muted
-            preload="auto"
-          />
-        </div>
-        <!-- stage 3 -->
-        <div class="hidden" v-if="stage2Loaded">
-          <img
-            :src="require('@/assets/images/backgrounds/' + (s3 + 9) + '.jpg')"
-            v-for="(s3, index) in 4"
-            @load="loadStage3('img ' + (s3 + 9))"
-            :key="'S3' + index"
-            alt=""
-          />
-          <video
-            :src="require('@/assets/videos/webm/' + (v3 + 9) + '.webm')"
-            v-for="(v3, index) in 4"
-            @loadeddata="loadStage3('video ' + (v3 + 9))"
-            :key="'V3' + index"
-            alt=""
-            muted
-            preload="auto"
-          />
-          <video
-            :src="require('@/assets/videos/end.webm')"
-            @loadeddata="loadStage3('ending')"
-            alt=""
-            muted
-            preload="auto"
-          />
-          <video
-            :src="require('@/assets/videos/outro/' + o + '.webm')"
-            v-for="(o, index) in ['Sveta', 'Tal', 'Maya', 'Diana']"
-            @loadeddata="loadStage3('outro ' + o)"
-            :key="'O' + index"
-            alt=""
-            muted
-            preload="auto"
-          />
-        </div>
+        <video
+          :src="require('@/assets/videos/intro.webm')"
+          @loadeddata="loadStage1('introbg')"
+          alt=""
+          muted
+          preload="auto"
+        />
+        <video
+          :src="require('@/assets/videos/webm/song.webm')"
+          @loadeddata="loadStage1('song')"
+          alt=""
+          muted
+          preload="auto"
+        />
+      </div>
+      <!-- stage 2 -->
+      <div class="hidden" v-if="stage1Loaded">
+        <img
+          :src="require('@/assets/images/backgrounds/' + (s2 + 4) + '.jpg')"
+          v-for="(s2, index) in 5"
+          @load="loadStage2('img ' + (s2 + 4))"
+          :key="'S2' + index"
+          alt=""
+        />
+        <video
+          :src="require('@/assets/videos/webm/' + (v2 + 4) + '.webm')"
+          v-for="(v2, index) in 5"
+          @loadeddata="loadStage2('video ' + (v2 + 4))"
+          :key="'V2' + index"
+          alt=""
+          muted
+          preload="auto"
+        />
+        <video
+          :src="require('@/assets/videos/thumbs/' + t + '.mp4')"
+          v-for="(t, index) in 13"
+          @loadeddata="loadStage2('thumbs ' + t)"
+          :key="'t' + index"
+          alt=""
+          muted
+          preload="auto"
+        />
+      </div>
+      <!-- stage 3 -->
+      <div class="hidden" v-if="stage2Loaded">
+        <img
+          :src="require('@/assets/images/backgrounds/' + (s3 + 9) + '.jpg')"
+          v-for="(s3, index) in 4"
+          @load="loadStage3('img ' + (s3 + 9))"
+          :key="'S3' + index"
+          alt=""
+        />
+        <video
+          :src="require('@/assets/videos/webm/' + (v3 + 9) + '.webm')"
+          v-for="(v3, index) in 4"
+          @loadeddata="loadStage3('video ' + (v3 + 9))"
+          :key="'V3' + index"
+          alt=""
+          muted
+          preload="auto"
+        />
+        <video
+          :src="require('@/assets/videos/end.webm')"
+          @loadeddata="loadStage3('ending')"
+          alt=""
+          muted
+          preload="auto"
+        />
+        <video
+          :src="require('@/assets/videos/outro/' + o + '.webm')"
+          v-for="(o, index) in ['Sveta', 'Tal', 'Maya', 'Diana']"
+          @loadeddata="loadStage3('outro ' + o)"
+          :key="'O' + index"
+          alt=""
+          muted
+          preload="auto"
+        />
       </div>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
